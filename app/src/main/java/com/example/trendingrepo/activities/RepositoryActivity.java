238638956ef74b0dd92892c2b9651464d83e5c09
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2019-10-07 23:40:11 +0530

    Add empty activity java class and layout file

---
+++ app/src/main/java/com/example/trendingrepo/activities/RepositoryActivity.java
@@ -0,0 +1,15 @@
+package com.example.trendingrepo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.trendingrepo.R;
+
+public class RepositoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+    }
+}
