f1d1f5c12ddac5808a8708a522b982eaca5e1f95
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2019-10-07 23:46:55 +0530

    Add layout structure of activity

--- app/src/main/java/com/example/trendingrepo/activities/RepositoryActivity.java
+++ app/src/main/java/com/example/trendingrepo/activities/RepositoryActivity.java
@@ -1,15 +1,22 @@
 package com.example.trendingrepo.activities;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.trendingrepo.R;
 
 public class RepositoryActivity extends AppCompatActivity {
 
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repository);
+        swipeRefreshLayout = findViewById(R.id.swipe_container);
+        recyclerView = findViewById(R.id.repo_list);
     }
 }
